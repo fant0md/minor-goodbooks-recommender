76ef16bca561b30148ce1c6ec6d3a48c36d2aa89
Yuriy Sosnin <sosnin.yuriy@gmail.com>
2021-05-14 19:50:48 +0300

    localisation+instructions

--- bot.py
+++ bot.py
@@ -34,13 +34,35 @@ pd.options.mode.chained_assignment = None
 #ratings = pd.read_csv('data/ratings.csv')
 book_map = pd.read_csv('data/books.csv')[['book_id', 'id', 'title', 'authors']]
 
-#with open('lightfm.pickle', 'rb') as f:
-#    lightfm = pickle.load(f)
 
-#with open('lightfm_hybrid.pickle', 'rb') as f:
-#    lightfm_hybrid = pickle.load(f)
+start_text = '''
+Здравствуйте. Я Ваш виртуальный помощник, осуществляющий рекомендацию книг.
 
-    
+Сейчас Вам будет предложено выбрать один из сценариев, на основе которых будет осуществляться подбор книг:
+
+* Dataset ID – любое число от 1 до 53424 Cледует выбрать этот вариант, если у Вас отсутствует аккаунт на * GoodReads, Вы не готовы тратить время на оценку уже прочитанных Вами книг и/или у Вас отсутствует читательский опыт
+
+* GoodReads ID – Ваш ID на сайте GoodReads
+
+* Custom Setup – Вы хотите получить рекомендацию на основе оценивания уже прочитанных Вами книг
+'''
+
+recommend_text = 'Выберите рекоммендательный алгоритм. Hybrid LightFM использует описание книг, поэтому благодаря ей рекомендации могут быть более адекватными, но стоит испытать обе рекомендательные системы'
+
+rating_text = '''
+Введите название книги, которую Вы желаете оценить, на английском. Система выдаст Вам полное название книги (имя автора + название) и предложит оценить книгу от 1 до 5. После оценки первой книги Вы сможете либо написать название ещё одной или нескольких книг для последующей оценки, либо выбрать клавишу «Finish» и перейти к получению рекомендации.
+'''
+
+scenario_text = 'Выберите сценарий'
+nouser_text = 'Пользователь не существует или не оценил ни одной книги из датасета. Попробуйте снова или выберите другой сценарий вызвав /start'
+gr_text = 'Укажите Ваш ID на сайте GoodRead
+askbook_text = 'Введите название книги (на английском) или закончите оценивание'
+nowrate_text = 'Теперь оцените книгу'
+wait_text = 'Это займет некоторое время'
+id_text = 'Введите число от 1 до ' + str(53424)
+wrongid_text = 'Неподходящий ввод. Введите число от 1 до ' + str(53424)
+
+
 def load_model(model_name):
     with open(str(model_name), 'rb') as f:
         model = pickle.load(f)
@@ -66,21 +88,21 @@ def start(update, context):
     context.user_data['user_ratings'] = None
     context.user_data['rated_dict'] = {}
     context.user_data['selected_book'] = None
-
-    text = "Choose scenario"
+    
+    update.message.reply_text(text=start_text, parse_mode='HTML')
+    
     buttons = [[
         'Dataset Id',
         'GoodReads Id',
         'Custom Setup'
     ]]
     keyboard = ReplyKeyboardMarkup(buttons, one_time_keyboard=True)
-    update.message.reply_text(text=text, reply_markup=keyboard)
+    update.message.reply_text(text=scenario_text, reply_markup=keyboard)
 
     return CHOOSING_SCENARIO
 
 
 def recommend(update, context):
-    text = "Choose recommender engine"
     buttons = [[
         # 'KNN',
         # 'SVD',
@@ -88,21 +110,19 @@ def recommend(update, context):
         'Hybrid LightFM'
     ]]
     keyboard = ReplyKeyboardMarkup(buttons, one_time_keyboard=True)
-    update.message.reply_text(text=text, reply_markup=keyboard)
+    update.message.reply_text(text=recommend_text, reply_markup=keyboard)
 
     return SELECTING_ENGINE
 
 
 def ask_user_id(update, context):
-    text = 'Type a number between 1 and ' + str(53424)
-    update.message.reply_text(text=text)
+    update.message.reply_text(text=id_text)
     
     return TYPING_USER
 
 
 def ask_user_id_again(update, context):
-    text = 'Wrong input. Type a number between 1 and ' + str(53424)
-    update.message.reply_text(text=text)
+    update.message.reply_text(text=wrongid_text)
     
     return TYPING_USER
 
@@ -117,15 +137,13 @@ def save_user_id(update, context):
 
 
 def ask_goodreads_id(update, context):
-    text = 'Type your GoodReads Id'
-    update.message.reply_text(text=text)
+    update.message.reply_text(text=gr_text)
     
     return TYPING_GOODREADS
 
 
 def ask_goodreads_id_again(update, context):
-    text = 'User does not exist or has no books from dataset rated. Please try again or choose different scenario by calling /start'
-    update.message.reply_text(text=text)
+    update.message.reply_text(text=nouser_text)
     
     return TYPING_GOODREADS
 
@@ -138,12 +156,17 @@ def save_goodreads_id(update, context):
     except ValueError:
         return ask_goodreads_id_again(update, context)
 
+    
+def inform_books_rating(update, context):
+    update.message.reply_text(text=rating_text)
+    
+    return ask_book_rating(update, context)
+
 
 def ask_book_rating(update, context):
-    text = 'Type the book title you want to rate or finish rating'
     buttons = [['Finish']]
     keyboard = ReplyKeyboardMarkup(buttons, one_time_keyboard=True)
-    update.message.reply_text(text=text, reply_markup=keyboard)
+    update.message.reply_text(text=askbook_text, reply_markup=keyboard)
     
     return TYPING_BOOK
 
@@ -163,7 +186,7 @@ def show_selected_book(update, context):
     update.message.reply_text(text=fancy_title(context.user_data['selected_book']),
                               parse_mode='HTML',
                               disable_web_page_preview=True)
-    update.message.reply_text(text='Now rate it', reply_markup=keyboard)
+    update.message.reply_text(text=nowrate_text, reply_markup=keyboard)
 
     return SELECTING_RATING
 
@@ -177,10 +200,8 @@ def save_book_rating(update, context):
 
 def rating_finished(update, context):
     books, rates = zip(*context.user_data['rated_dict'].items())
-    # book_map = pd.read_csv('data/books.csv')[['id', 'title', 'authors']]
-    # book_ids = [book_map.loc[book_map['title'] == i, 'id'].values[0] for i in books]
     book_ids = book_map['id'][book_map['title'].isin(books)].values
-    # book_ids = book_map.id.values[np.isin(book_map.title, books)]
+
     context.user_data['user_ratings'] = pd.DataFrame(
         {'user_id': np.repeat(0, len(book_ids)), 'book_id': book_ids, 'rating': rates})
 
@@ -203,14 +224,14 @@ def rating_finished(update, context):
 
 def rec_lightfm(update, context):
     lightfm = load_model('lightfm.pickle')
-    warn = update.message.reply_text(text='This takes time')
+    warn = update.message.reply_text(text=wait_text)
     reclist = lightfm.predict_list(context.user_data['user_ratings'])
     warn.edit_text(text=fancy_list(reclist), parse_mode='HTML', disable_web_page_preview=True)
 
 
 def rec_lightfm_hybrid(update, context):
     lightfm_hybrid = load_model('lightfm_hybrid.pickle')
-    warn = update.message.reply_text(text='This takes time')
+    warn = update.message.reply_text(text=wait_text)
     reclist = lightfm_hybrid.predict_list(context.user_data['user_ratings'])
     warn.edit_text(text=fancy_list(reclist), parse_mode='HTML', disable_web_page_preview=True)
 
@@ -221,7 +242,7 @@ convhandler = ConversationHandler(
         CHOOSING_SCENARIO: [
             MessageHandler(Filters.regex('^Dataset Id$'), ask_user_id),
             MessageHandler(Filters.regex('^GoodReads Id$'), ask_goodreads_id),
-            MessageHandler(Filters.regex('^Custom Setup$'), ask_book_rating)
+            MessageHandler(Filters.regex('^Custom Setup$'), inform_books_rating)
         ],
         TYPING_USER: [
             MessageHandler(Filters.text & ~Filters.command, save_user_id)
